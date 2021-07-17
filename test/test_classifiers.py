dea3ea4970c0f2200879446d089cae6fd6480358
bofeng1997 <bofeng1997@gamil.com>
2021-07-18 01:29:20 +0800

    add b2g4 vector

--- test/test_classifiers.py
+++ test/test_classifiers.py
@@ -1,7 +1,7 @@
 import torch
 from mmcls.models.classifiers import ImageClassifier
-from mmcls.models.backbones.RepVGG import g4_map,RepVGG
-
+from mmcls.models.backbones.RepVGG import RepVGG
+g4_map = {l: 4 for l in [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26]}
 
 def test_image_classifier_RepVGG():
     model_cfg = dict(
