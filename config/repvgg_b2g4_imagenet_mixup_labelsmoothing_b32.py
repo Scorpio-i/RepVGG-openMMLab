3382de775d1ef9ce397e2c8ac67f620331290337
bofeng1997 <bofeng1997@gamil.com>
2021-07-18 23:53:43 +0800

    fixup

--- config/repvgg_b2g4_imagenet_mixup_labelsmoothing_b32.py
+++ config/repvgg_b2g4_imagenet_mixup_labelsmoothing_b32.py
@@ -10,18 +10,21 @@ model = dict(
     type='ImageClassifier',
     backbone=dict(type='RepVGG',num_blocks=[4, 6, 16, 1], width_multiplier=[2.5, 2.5, 2.5, 5],override_groups_map=g4_map),
     neck=None,
-    head=dict(type='LinearClsHead',num_classes=10,in_channels=2560,loss=dict(type='CrossEntropyLoss', loss_weight=1.0),topk=(1, 5)
-            ))
+    head=dict(type='LinearClsHead',num_classes=1000,in_channels=2560,loss=dict(
+            type='LabelSmoothLoss', label_smooth_val=0.1, loss_weight=1.0),
+            topk=(1, 5)),
+    train_cfg=dict(
+        augments=dict(type='BatchMixup', alpha=0.2, num_classes=1000,
+                      prob=1.))
+            )
 
-
-#cifar_bs_16
-dataset_type = 'CIFAR10'
+# dataset settings
+dataset_type = 'ImageNet'
 img_norm_cfg = dict(
-    mean=[125.307, 122.961, 113.8575],
-    std=[51.5865, 50.847, 51.255],
-    to_rgb=False)
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='RandomCrop', size=32, padding=4),
+    dict(type='LoadImageFromFile'),
+    dict(type='RandomResizedCrop', size=224),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
@@ -29,32 +32,40 @@ train_pipeline = [
     dict(type='Collect', keys=['img', 'gt_label'])
 ]
 test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', size=(256, -1)),
+    dict(type='CenterCrop', crop_size=224),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
     dict(type='Collect', keys=['img'])
 ]
 data = dict(
-    samples_per_gpu=16,
+    samples_per_gpu=32,
     workers_per_gpu=2,
     train=dict(
-        type=dataset_type, data_prefix='data/cifar10',
+        type=dataset_type,
+        data_prefix='data/imagenet/train',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        data_prefix='data/cifar10',
-        pipeline=test_pipeline,
-        test_mode=True),
+        data_prefix='data/imagenet/val',
+        ann_file='data/imagenet/meta/val.txt',
+        pipeline=test_pipeline),
     test=dict(
+        # replace `data/val` with `data/test` for standard test
         type=dataset_type,
-        data_prefix='data/cifar10',
-        pipeline=test_pipeline,
-        test_mode=True))
+        data_prefix='data/imagenet/val',
+        ann_file='data/imagenet/meta/val.txt',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='accuracy')
+
+
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='step', step=[100, 150])
+lr_config = dict(policy='step', step=[30, 60, 90])
 runner = dict(type='EpochBasedRunner', max_epochs=200)
 
 # checkpoint saving
