a9f2b6204aa07e7b19a58560750aa0bed4beec2d
bofeng1997 <bofeng1997@gamil.com>
2021-07-21 02:18:43 +0800

    pep8 style

--- config/repvggb3g4_b32x8.py
+++ config/repvggb3g4_b32x8.py
@@ -7,29 +7,22 @@ RepVGG(num_blocks=[4, 6, 16, 1],
 
 g4_map = {l: 4 for l in [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26]}
 
-
-model = dict(
-    type='ImageClassifier',
-    backbone=dict(
-        type='RepVGG',
-        num_classes=1000,
-        num_blocks=[4, 6, 16, 1],
-        width_multiplier=[3, 3, 3, 5],
-        override_groups_map=g4_map
-        ),
-    neck=None,
-    head=dict(
-        type='ClsHead',
-        loss=dict(
-            type='CrossEntropyLoss',
-            loss_weight=1.0),
-        topk=(1, 5)
-        ))
+model = dict(type='ImageClassifier',
+             backbone=dict(type='RepVGG',
+                           num_classes=1000,
+                           num_blocks=[4, 6, 16, 1],
+                           width_multiplier=[3, 3, 3, 5],
+                           override_groups_map=g4_map),
+             neck=None,
+             head=dict(type='ClsHead',
+                       loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+                       topk=(1, 5)))
 
 # dataset settings
 dataset_type = 'ImageNet'
-img_norm_cfg = dict(
-    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], to_rgb=True)
+img_norm_cfg = dict(mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                    to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='RandomResizedCrop', size=224),
@@ -50,15 +43,13 @@ test_pipeline = [
 data = dict(
     samples_per_gpu=32,
     workers_per_gpu=2,
-    train=dict(
-        type=dataset_type,
-        data_prefix='data/imagenet/train',
-        pipeline=train_pipeline),
-    val=dict(
-        type=dataset_type,
-        data_prefix='data/imagenet/val',
-        ann_file='data/imagenet/meta/val.txt',
-        pipeline=test_pipeline),
+    train=dict(type=dataset_type,
+               data_prefix='data/imagenet/train',
+               pipeline=train_pipeline),
+    val=dict(type=dataset_type,
+             data_prefix='data/imagenet/val',
+             ann_file='data/imagenet/meta/val.txt',
+             pipeline=test_pipeline),
     test=dict(
         # replace `data/val` with `data/test` for standard test
         type=dataset_type,
@@ -66,8 +57,6 @@ data = dict(
         ann_file='data/imagenet/meta/val.txt',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='accuracy')
-
-
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
