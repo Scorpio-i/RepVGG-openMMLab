58a90744ba1d5c9bc16982a29f5d2465be992e85
bofeng1997 <bofeng1997@gamil.com>
2021-07-21 02:30:08 +0800

    pep8 style

--- test/test_classifiers.py
+++ test/test_classifiers.py
@@ -4,29 +4,24 @@ from mmcls.models.classifiers import ImageClassifier
 
 def test_repvgg():
     g4_map = {l: 4 for l in [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26]}
-    model_cfg = dict(
-        backbone=dict(
-            type='RepVGG',
-            num_classes=1000,
-            num_blocks=[4, 6, 16, 1],
-            width_multiplier=[2.5, 2.5, 2.5, 5],
-            override_groups_map=g4_map,
-            ),
-        neck=None,
-        head=dict(
-            type='ClsHead',
-            loss=dict(type='CrossEntropyLoss'))
-
-            )
+    model_cfg = dict(backbone=dict(
+        type='RepVGG',
+        num_classes=1000,
+        num_blocks=[4, 6, 16, 1],
+        width_multiplier=[2.5, 2.5, 2.5, 5],
+        override_groups_map=g4_map,
+    ),
+                     neck=None,
+                     head=dict(type='ClsHead',
+                               loss=dict(type='CrossEntropyLoss')))
     img_classifier = ImageClassifier(**model_cfg)
     img_classifier.init_weights()
     imgs = torch.randn(16, 3, 32, 32)
     label = torch.randint(0, 10, (16, ))
 
     losses = img_classifier.forward_train(imgs, label)
-    assert losses['loss'].item() > 0 ,"====> loss error "
+    assert losses['loss'].item() > 0, "====> loss error "
 
 
-
-# if __name__ == '__main__':
-#     test_repvgg()
+if __name__ == '__main__':
+    test_repvgg()
