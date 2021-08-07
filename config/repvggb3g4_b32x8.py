6551d0667e61c27f8c6af56a9a59aff5fa211e73
bofeng1997 <bofeng1997@gmail.com>
2021-08-08 03:51:48 +0800

    bug fixed

--- config/repvggb3g4_b32x8.py
+++ config/repvggb3g4_b32x8.py
@@ -25,7 +25,9 @@ img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
                     to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RandomResizedCrop', size=224),
+    dict(type='RandomResizedCrop', size=224,
+        backend='pillow',
+        interpolation='bicubic'),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
@@ -34,8 +36,10 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', size=(256, -1)),
-    dict(type='CenterCrop', crop_size=256),
+    dict(type='Resize', size=(256, -1),
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='CenterCrop', crop_size=224),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
     dict(type='Collect', keys=['img'])
